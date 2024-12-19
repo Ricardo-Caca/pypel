fcd661bfedfda0e5e312322193c5babaed8c6119
Ricardo Caca <ricardo.antonio.caca@gmail.com>
2024-12-19 15:43:28 -0300

    views e rotas auth/core

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'autenticacao'
 
 urlpatterns = [
-    path('', views.login, name='login')
+    path('', views.login, name='login'),
+    path('get_perfis/', views.get_perfis, name='get_perfis'),
+    path('logout/', views.logout, name='logout'),
 ]
